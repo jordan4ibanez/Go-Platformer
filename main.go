8ba5f42933e809bde2ea1cbc30809e1ff952794d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-11-29 22:25:24 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	"fmt"
-	test "github.com/jordan4ibanez/Go-Platformer/src"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
 func main() {
-
-	fmt.Println("test")
-
-	test.TestCode()
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
