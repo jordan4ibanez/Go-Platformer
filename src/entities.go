1fda242ee788ac706f2788ba155144a41a3ab4e7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-01 17:19:17 -0500

    Implement inertia getters and setters

--- src/entities.go
+++ src/entities.go
@@ -43,3 +43,31 @@ func SetPositionX(index int, x float64) {
 func SetPositionY(index int, y float64) {
 	position[index][1] = y
 }
+
+//inertia getters
+
+func GetInertia(index int) [2]float64 {
+	return inertia[index]
+}
+
+func GetInertiaX(index int) float64 {
+	return inertia[index][0]
+}
+
+func GetInertiaY(index int) float64 {
+	return inertia[index][1]
+}
+
+//inertia setters
+
+func SetInertia(index int, x float64, y float64) {
+	inertia[index] = [2]float64{x, y}
+}
+
+func SetInertiaX(index int, x float64) {
+	inertia[index][0] = x
+}
+
+func SetInertiaY(index int, y float64) {
+	inertia[index][1] = y
+}
