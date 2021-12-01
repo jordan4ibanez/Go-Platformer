8ea6e4731d2df944a85da142766ef2b7523a3a73
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-01 17:05:47 -0500

    Organize getters/setters better

--- src/entities.go
+++ src/entities.go
@@ -4,6 +4,8 @@ var static [2]bool = [2]bool{false, true}
 
 var position [2][2]float64 = [2][2]float64{{0.0, 0.0}, {50.0, 0.0}} //this must be defined inline for some reason
 
+//static getters and setters
+
 func getStatic(index int) bool {
 	return static[index]
 }
@@ -12,18 +14,30 @@ func setStatic(index int, state bool) {
 	static[index] = state
 }
 
+//position getters
+
 func getPosition(index int) [2]float64 {
 	return position[index]
 }
 
-func getX(index int) float64 {
+func getPositionX(index int) float64 {
 	return position[index][0]
 }
 
-func getY(index int) float64 {
+func getPositionY(index int) float64 {
 	return position[index][1]
 }
 
+//position setters
+
 func setPosition(index int, x float64, y float64) {
 	position[index] = [2]float64{x, y}
+}
+
+func setPositionX(index int, x float64) {
+	position[index][0] = x
+}
+
+func setPositionY(index int, y float64) {
+	position[index][1] = y
 }
