aa75dac9d139c8916695975fd2a2d0bb81dbce0b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-01 17:08:22 -0500

    Make all functions exportable

--- src/entities.go
+++ src/entities.go
@@ -1,6 +1,6 @@
 package engine
 
-var static [2]bool = [2]bool{false, true}
+var static [2]bool = [2]bool{false, true} //if entity is static, it cannot move
 
 var position [2][2]float64 = [2][2]float64{{0.0, 0.0}, {50.0, 0.0}} //this must be defined inline for some reason
 
@@ -8,38 +8,38 @@ var inertia [2][2]float64 = [2][2]float64{{0.0, 0.0}, {0.0, 0.0}} //this must be
 
 //static getters and setters
 
-func getStatic(index int) bool {
+func GetStatic(index int) bool {
 	return static[index]
 }
 
-func setStatic(index int, state bool) {
+func SetStatic(index int, state bool) {
 	static[index] = state
 }
 
 //position getters
 
-func getPosition(index int) [2]float64 {
+func GetPosition(index int) [2]float64 {
 	return position[index]
 }
 
-func getPositionX(index int) float64 {
+func GetPositionX(index int) float64 {
 	return position[index][0]
 }
 
-func getPositionY(index int) float64 {
+func GetPositionY(index int) float64 {
 	return position[index][1]
 }
 
 //position setters
 
-func setPosition(index int, x float64, y float64) {
+func SetPosition(index int, x float64, y float64) {
 	position[index] = [2]float64{x, y}
 }
 
-func setPositionX(index int, x float64) {
+func SetPositionX(index int, x float64) {
 	position[index][0] = x
 }
 
-func setPositionY(index int, y float64) {
+func SetPositionY(index int, y float64) {
 	position[index][1] = y
 }
