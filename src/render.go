dadcff97e4f630531ed9d679340872079a478a9a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-01 18:07:19 -0500

    Fix issue with V1 being imported

--- src/render.go
+++ src/render.go
@@ -1,12 +1,12 @@
 package engine
 
 import (
-	"image/color"
+	//"image/color"
 
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2"
+	//"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 func DrawEntities(screen *ebiten.Image) {
-	ebitenutil.DrawRect(screen, 0, 0, 20, 20, color.White)
+	//ebitenutil.DrawRect(screen, 0, 0, 20, 20, color.White)
 }
