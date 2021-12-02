9c46bfa4a1886c8056531e562bc6048e0c3631a6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-02 00:52:41 -0500

    Implement map collision detection

--- src/physics.go
+++ src/physics.go
@@ -8,6 +8,7 @@ var workerPosition2 [2]float64 = [2]float64{}
 var workerSize [2]float64 = [2]float64{}
 var workerSize2 [2]float64 = [2]float64{}
 var workerInertia [2]float64 = [2]float64{}
+var workerTile [2]int = [2]int{}
 
 //basically is the inverse of inclusive AABB - faster
 func exclusionAABB(workerX float64, workerY float64) bool {
@@ -97,6 +98,89 @@ func collisionDetectY(index int, workerX float64, workerY float64, inertiaY floa
 	return inertiaY
 }
 
+func collisionDetectXMap(index int, workerX float64, workerY float64, inertiaX float64) float64 {
+
+	workerTile = [2]int{int(workerX / 32), int(workerY / 32)}
+
+	for x := workerTile[0] - 1; x <= workerTile[0]+1; x++ {
+		for y := workerTile[1] - 1; y <= workerTile[1]+1; y++ {
+
+			//don't allow to go out of bounds
+			if x >= 0 && x < GetMapLength() && y >= 0 && y < GetMapHeight() {
+
+				//collide with the tiles
+				if GameMap[y][x] > 0 {
+
+					if inertiaX < 0 {
+						workerPosition2 = [2]float64{float64(x) * 32, float64(y) * 32}
+						workerSize2 = [2]float64{32, 32}
+
+						if exclusionAABB(workerX, workerY) {
+							workerPosition[0] = workerPosition2[0] + workerSize2[0] + 0.005
+							inertiaX = 0
+						}
+					} else if inertiaX > 0 {
+
+						workerPosition2 = [2]float64{float64(x) * 32, float64(y) * 32}
+						workerSize2 = [2]float64{32, 32}
+
+						if exclusionAABB(workerX, workerY) {
+							//fmt.Println("collide + x") //debug
+							workerPosition[0] = workerPosition2[0] - workerSize[0] - 0.005
+							inertiaX = 0
+						}
+
+					}
+				}
+			}
+		}
+	}
+
+	return inertiaX
+}
+
+
+func collisionDetectYMap(index int, workerX float64, workerY float64, inertiaY float64) float64 {
+
+	workerTile = [2]int{int(workerX / 32), int(workerY / 32)}
+
+	for x := workerTile[0] - 1; x <= workerTile[0]+1; x++ {
+		for y := workerTile[1] - 1; y <= workerTile[1]+1; y++ {
+
+			//don't allow to go out of bounds
+			if x >= 0 && x < GetMapLength() && y >= 0 && y < GetMapHeight() {
+
+				//collide with the tiles
+				if GameMap[y][x] > 0 {
+
+					if inertiaY < 0 {
+						workerPosition2 = [2]float64{float64(x) * 32, float64(y) * 32}
+						workerSize2 = [2]float64{32, 32}
+
+						if exclusionAABB(workerX, workerY) {
+							workerPosition[1] = workerPosition2[1] + workerSize2[1] + 0.005
+							inertiaY= 0
+						}
+					} else if inertiaY > 0 {
+
+						workerPosition2 = [2]float64{float64(x) * 32, float64(y) * 32}
+						workerSize2 = [2]float64{32, 32}
+
+						if exclusionAABB(workerX, workerY) {
+							//fmt.Println("collide + x") //debug
+							workerPosition[1] = workerPosition2[1] - workerSize[1] - 0.005
+							inertiaY = 0
+						}
+
+					}
+				}
+			}
+		}
+	}
+
+	return inertiaY
+}
+
 func applyInertia(index int, dtime float64) {
 	workerPosition = GetPosition(index)
 
@@ -106,22 +190,32 @@ func applyInertia(index int, dtime float64) {
 
 	workerPosition[0] = workerPosition[0] + (workerInertia[0] * dtime)
 
-	//detect X
+	//detect X entities
 
 	workerInertia[0] = collisionDetectX(index, workerPosition[0], workerPosition[1], workerInertia[0])
 
+	//detect X map tiles
+
+	workerInertia[0] = collisionDetectXMap(index, workerPosition[0], workerPosition[1], workerInertia[0])
+
 	//move Y
+
 	workerPosition[1] = workerPosition[1] + (workerInertia[1] * dtime)
 
-	//detect Y
+	//detect Y entities
 
 	workerInertia[1] = collisionDetectY(index, workerPosition[0], workerPosition[1], workerInertia[1])
+
+	//detect Y map tiles
+
+	workerInertia[1] = collisionDetectYMap(index, workerPosition[0], workerPosition[1], workerInertia[1])
 
 	//set in memory
 
 	SetPosition(index, workerPosition[0], workerPosition[1])
 
 	SetInertia(index, workerInertia[0], workerInertia[1])
+
 }
 
 func RunPhysics(delta float64) {
